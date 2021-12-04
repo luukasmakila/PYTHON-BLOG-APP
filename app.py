4ba243fa2e137e890d62967b7ddb3338587aa043
Luukas <luukas.makila@kolumbus.fi>
2021-12-04 21:39:50 +0200

    added auth and views files

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from website import create_app
 
 #run this file to start the website
 
-app = create_app()
-
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -6,4 +6,10 @@ def create_app():
     app = Flask(__name__)
     app.secret_key = "adasfdsfgsv"
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+    
     return app
