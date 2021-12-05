7fa45b8c86b43bed585fb31a3ccc2fb3dcc3db34
Luukas <luukas.makila@kolumbus.fi>
2021-12-05 22:26:37 +0200

    allowed POST requests and getting user inputs

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,25 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 
 #routes related to authentication
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    print(email)
     return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
     return redirect(url_for("views.home")) #redirects user to the home page
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    password2 = request.form.get("password2")
+    print(username)
     return render_template("sign_up.html")
