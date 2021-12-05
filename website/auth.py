1282e2856365a5a3298f9116acc2cf06cbe2157c
Luukas <luukas.makila@kolumbus.fi>
2021-12-05 23:13:01 +0200

    added input checks and message flashing

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 
 #routes related to authentication
 
@@ -8,7 +8,17 @@ auth = Blueprint("auth", __name__)
 def login():
     email = request.form.get("email")
     password = request.form.get("password")
-    print(email)
+
+    database = ["luukas@gmail.com", "ltd"]
+
+    if email not in database:
+        flash("No users with the given email!", category="error")
+    if not password: #if the password is not correct
+        flash("Wrong password!", category="error")
+    else:
+        #log the user in
+        flash("Successfully logged in!", category="success")
+    
     return render_template("login.html")
 
 @auth.route("/logout")
@@ -17,9 +27,29 @@ def logout():
 
 @auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
-    email = request.form.get("email")
-    username = request.form.get("username")
-    password = request.form.get("password")
-    password2 = request.form.get("password2")
-    print(username)
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password2 = request.form.get("password2")
+
+        database = ["luukas@gmail.com", "ltd"]
+
+        if email in database:
+            flash("Email already exits!", category="error")
+        if username in database:
+            flash("Username already exits!", category="error")
+        if len(email) < 7:
+            flash("Email is too short!", category="error")
+        if len(username) < 3:
+            flash("Username is too short!", category="error")
+        if len(password) < 5:
+            flash("Password is too short!", category="error")
+        if password != password2:
+            flash("Passwords don't match!", category="error")
+        else:
+            #add user to the database
+            flash("User created!", category="success")
+    
     return render_template("sign_up.html")
