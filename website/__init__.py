9da4c8d3b27c127167b2e3b4f82c351edee703f2
Luukas <luukas.makila@kolumbus.fi>
2021-12-06 22:19:13 +0200

    created sqlite database

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,7 @@ DB = "database.db"
 def create_app():
     app = Flask(__name__)
     app.secret_key = "adasfdsfgsv"
-    app.config["SQL_ALCHEMY_DATABASE_URI"] = f"sqlite://{DB}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB}"
     db.init_app(app)
 
     from .views import views
